322e584cb883f39629ba42493446ebbe5fdc1f32
Dungla31 <leanhdung3105@gmail.com>
2025-06-11 17:15:13 +0700

    Thêm sản phẩm vào giỏ khi đã đăng xuất

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
+
+from carts.models import Cart, CartItem
+from carts.views import _cart_id
 from .forms import RegistrationForm
 from .models import Account
 from django.contrib import messages, auth
@@ -10,6 +13,7 @@ from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.utils.encoding import force_bytes, force_str
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import EmailMessage
+import requests
 
 def register(request):
     if request.method == 'POST':
@@ -28,7 +32,7 @@ def register(request):
 
             # USER ACTIVATION
             current_site = get_current_site(request)
-            mail_subject = 'Please activate your account'
+            mail_subject = 'Vui xác thực tài khoản của bạn'
             message = render_to_string('accounts/account_verification_email.html', {
                 'user': user,
                 'domain': current_site,
@@ -53,14 +57,62 @@ def login(request):
         password = request.POST['password']
 
         user = auth.authenticate(email=email, password=password)
+
         if user is not None:
+            try:
+                cart = Cart.objects.get(cart_id=_cart_id(request))
+                is_cart_item_exists = CartItem.objects.filter(cart=cart).exists()
+                if is_cart_item_exists:
+                    cart_item = CartItem.objects.filter(cart=cart)
+
+                    # Getting the product variations by cart id
+                    product_variation = []
+                    for item in cart_item:
+                        variation = item.variations.all()
+                        product_variation.append(list(variation))
+
+                    # Get the cart items from the user to access his product variations
+                    cart_item = CartItem.objects.filter(user=user)
+                    ex_var_list = []
+                    id = []
+                    for item in cart_item:
+                        existing_variation = item.variations.all()
+                        ex_var_list.append(list(existing_variation))
+                        id.append(item.id)
+
+                    # product_variation = [1, 2, 3, 4, 6]
+                    # ex_var_list = [4, 6, 3, 5]
+
+                    for pr in product_variation:
+                        if pr in ex_var_list:
+                            index = ex_var_list.index(pr)
+                            item_id = id[index]
+                            item = CartItem.objects.get(id=item_id)
+                            item.quantity += 1
+                            item.user = user
+                            item.save()
+                        else:
+                            cart_item = CartItem.objects.filter(cart=cart)
+                            for item in cart_item:
+                                item.user = user
+                                item.save()
+            except:
+                pass
             auth.login(request, user)
-            messages.success(request, "Đăng nhập thành công.")
-            return redirect('dashboard')
+            messages.success(request, 'Bạn đã đăng nhập thành công.')
+            url = request.META.get('HTTP_REFERER')
+            try:
+                query = requests.utils.urlparse(url).query
+                # next=/cart/checkout/
+                params = dict(x.split('=') for x in query.split('&'))
+                if 'next' in params:
+                    nextPage = params['next']
+                    return redirect(nextPage)
+            except:
+                return redirect('dashboard')
         else:
-            messages.error(request, "Đăng nhập thất bại. Vui lòng kiểm tra lại email và mật khẩu.")
+            messages.error(request, 'Thông tin đăng nhập không hợp lệ')
             return redirect('login')
-            
     return render(request, 'accounts/login.html')
 
 @login_required(login_url = 'login')
@@ -98,7 +150,7 @@ def forgotPassword(request):
 
             # Reset password email
             current_site = get_current_site(request)
-            mail_subject = 'Reset Your Password' 
+            mail_subject = 'Đặt lại mật khẩu của bạn'
             message = render_to_string('accounts/reset_password_email.html', {
                 'user': user,
                 'domain': current_site,
@@ -112,7 +164,7 @@ def forgotPassword(request):
             messages.success(request, 'Password reset email has been sent to your email address.')
             return redirect('login')
         else:
-            messages.error(request, 'Account does not exist!')
+            messages.error(request, 'Tài khoản không tồn tại!')
             return redirect('forgotPassword')
     return render(request, 'accounts/forgotPassword.html')
 
@@ -126,10 +178,10 @@ def resetpassword_validate(request, uidb64, token):
 
     if user is not None and default_token_generator.check_token(user, token):
         request.session['uid'] = uid
-        messages.success(request, 'Please reset your password')
+        messages.success(request, 'Vui lòng đặt lại mật khẩu của bạn')
         return redirect('resetPassword')
     else:
-        messages.error(request, 'This link has been expired!')
+        messages.error(request, 'Liên kết này đã hết hạn!')
         return redirect('login')
 
 
@@ -143,10 +195,10 @@ def resetPassword(request):
             user = Account.objects.get(pk=uid)
             user.set_password(password)
             user.save()
-            messages.success(request, 'Password reset successful')
+            messages.success(request, 'Đặt lại mật khẩu thành công')
             return redirect('login')
         else:
-            messages.error(request, 'Password do not match!')
+            messages.error(request, 'Mật khẩu không khớp!')
             return redirect('resetPassword')
     else:
         return render(request, 'accounts/resetPassword.html')
--- store/views.py
+++ store/views.py
@@ -14,13 +14,13 @@ def store(request, category_slug=None):
     if category_slug != None:
         categories = get_object_or_404(Category, slug = category_slug)
         products = Product.objects.all().filter(category = categories,is_available=True)
-        paginator = Paginator(products, 1)
+        paginator = Paginator(products, 5)
         page = request.GET.get('page')
         paged_products = paginator.get_page(page)
         product_count = products.count()
     else:
         products = Product.objects.all().filter(is_available=True).order_by('id')
-        paginator = Paginator(products, 3)
+        paginator = Paginator(products, 5)
         page = request.GET.get('page')
         paged_products = paginator.get_page(page)
         product_count = products.count()
